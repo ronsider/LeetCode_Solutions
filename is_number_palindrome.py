6f0617d0870faf25e9649f81ce7d49d09e3646f6
ronsider <ronsider2@gmail.com>
2022-10-05 23:38:31 +0300

    python solution

--- is_number_palindrome.py
+++ is_number_palindrome.py
@@ -1,9 +1,18 @@
-from cmath import log10
+from math import log10
 from math import floor
 
 
 def is_number_palindrome(number):
     if(number/10 == 0):
         return True
-    number_of_digits=floor(log10(number))+1
-    
+    number_of_digits=int(log10(number))+1
+    while(number):
+        lmd = int(number / pow(10,number_of_digits-1))
+        rmd = number % 10
+        if(lmd != rmd):
+            return False
+        number = number-pow(10,number_of_digits-1)*lmd
+        number = int(number/10)
+        number_of_digits-=2    
+    return True
+
