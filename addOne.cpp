9e1ff5b84415b2439322ac4b63d0f3772b124732
ronsider <ronsider2@gmail.com>
2022-10-12 23:52:30 +0300

    add one to array representing number

---
+++ addOne.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using std::vector;
+
+
+vector<int> digits(vector<int>& number)
+{
+    const std::size_t last_position=number.size()-1;
+    
+    if(number[last_position]<9)
+    
+        number[last_position]++;
+    
+    else
+    {
+        number.reserve(number.size()+1);
+        std::reverse(number.begin(),number.end());
+        number.push_back(0);
+        std::reverse(number.begin(),number.end());
+        bool carry{true};
+        int temp;
+        for(auto it=number.rbegin();it!=number.rend();it++)
+        {
+            if(carry && (*it)==9)
+            {
+                *it=0;
+                carry=true;
+                continue;
+            }
+            
+            if(carry && *it!=9)
+            {
+                carry=false;
+                (*it)+=1;
+                break;
+            }
+        }
+        
+    }
+    return number;
+
+
+}
+
