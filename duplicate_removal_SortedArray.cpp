4278aa6340a271c4ea35bb717fd916bade053bb8
ronsider <ronsider2@gmail.com>
2022-10-30 11:14:07 +0200

    remove duplicates from sorted vector

--- duplicate_removal_SortedArray.cpp
+++ duplicate_removal_SortedArray.cpp
@@ -1,2 +1,10 @@
 #include <vector>
+#include <algorithm>
 using std::vector;
+
+void remove_duplicates(vector<int>& numbers)
+{
+    auto it=std::unique(numbers.begin(),numbers.end());
+    numbers.erase(it,numbers.end());
+}
+
