af3447091aa3a0432a4d400952acf4356d41a49d
ronsider <ronsider2@gmail.com>
2022-11-01 23:50:40 +0200

    main function

--- duplicate_removal_SortedArray.cpp
+++ duplicate_removal_SortedArray.cpp
@@ -12,13 +12,30 @@ void remove_duplicates(vector<int>& numbers)
 //** add pure implementation: **/
 void remove_duplicate_pure(vector<int>& numbers)
 {
+    std::size_t temp{};
+    for(std::size_t i=0;i<numbers.size()-1;i++)
+    {
+        if(numbers[i]==numbers[i+1])
+        {
+            temp=i+1;
+            while(numbers[i]==numbers[temp])
+            {
+                numbers[temp]=-999;
+                temp++;
+            }
+        }
+        if(i!=numbers.size()-2)
+        {
+        i=temp+1;
+        }
+    }
 
 }
 
 int main()
 {
     vector<int>numbers{1,2,2,3,4,4,5,6,7,7,8};
-    remove_duplicates(numbers);
+    remove_duplicate_pure(numbers);
     auto print{[&](){
         for(const auto& i:numbers)
         {
