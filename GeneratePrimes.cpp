9f619b73b7f237f5a87f86544a9f5381c963ca1f
ronsider <ronsider2@gmail.com>
2022-11-22 21:45:08 +0200

    add solution for number that is not within range

--- GeneratePrimes.cpp
+++ GeneratePrimes.cpp
@@ -1,9 +1,47 @@
 #include <vector>
+#include <algorithm>
+#include <cmath>
 #include <iostream>
 using std::vector;
 
-vector<int>generate_primes(const int number)
+vector<int>prime_numbers(const int number)
 {
-    vector<int>_primes{2,3,5,7,11,13,17,19,23,29,31};
-    
+    vector<int>primeNumbers{2,3,5,7,11,13,17,19,23,29,31};
+    auto position=std::lower_bound(primeNumbers.begin(),primeNumbers.end(),number);
+    if(position!=primeNumbers.end())
+    {
+        vector<int>result(primeNumbers.begin(),position);
+        return result;
+    }
+    else
+    {
+        int last_element=primeNumbers.back()+2;
+        while(last_element<=number)
+        {
+            int temp=(int)sqrt(last_element);
+            bool flag=true;
+            auto _position=std::lower_bound(primeNumbers.begin(),primeNumbers.end(),temp);
+            for(auto pos=primeNumbers.begin();pos<=_position;pos++)
+            {
+                if(last_element%(*pos)==0)
+                {
+                    flag=false;
+                  break;
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+            if(flag)
+            {
+                primeNumbers.push_back(last_element);
+            }
+            last_element+=2;
+        }
+
+        return primeNumbers;
+    }
+
 }
