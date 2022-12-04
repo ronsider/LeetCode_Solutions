e44a34faea788d59bf2be980d97b77ecf2b56be5
ronsider <ronsider2@gmail.com>
2022-12-04 22:22:34 +0200

    some minor changes

--- appPermutation.py
+++ appPermutation.py
@@ -1,22 +1,15 @@
-# def swap_elements(element1,element2):
-#     element1, element2=element2, element1
-
-
-
-def permutation(permutation_order, letters):
-    for i in range(len(permutation_order)):
-        if(permutation_order[i]==i):
+def func(perm,lst):
+    for i in range(len(perm)):
+        if(perm[i]==i):
             continue
-        #swap_elements(letters[permutation_order[i]],letters[i])
-        letters[permutation_order[i]],letters[i]=letters[i],letters[permutation_order[i]]
-        #swap_elements(permutation_order[i],permutation_order[permutation_order[i]])
-        permutation_order[i],permutation_order[permutation_order[i]]=permutation_order[permutation_order[i]],permutation_order[i]
+        lst[perm[i]],lst[i]=lst[i],lst[perm[i]]
+        perm[i],perm[perm[i]]=perm[perm[i]],perm[i]
         i=0
-        
 
-lst=['a','b','c','d','e']
-pers=[3,4,2,1,0]
-permutation(pers,lst)
-for x in lst:
-    print(x)   
+lst = ['a','b','c','d']
+perm = [2,0,1,3]
+
+func(perm,lst)
+
+print(perm)
 
