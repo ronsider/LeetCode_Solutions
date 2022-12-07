293536f43632291eb3492c8471ceea72f93fe639
ronsider <ronsider2@gmail.com>
2022-12-07 23:51:10 +0200

    functions

---
+++ check_sudoku_valid.cpp
@@ -0,0 +1,19 @@
+#include <array>
+using std::array;
+
+
+ constexpr static std::array<int,9>numbers{1,2,3,4,5,6,7,8,9};
+
+constexpr bool check_mini_grid(const array<array<int,3>,3>& grid)
+{
+    
+}
+constexpr bool check_sudoku()
+{
+
+}
+int main()
+{
+  
+  
+}
