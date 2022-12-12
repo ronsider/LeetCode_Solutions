5b0043d5c6751333cf37489686b368462b25150c
ronsider <ronsider2@gmail.com>
2022-12-12 22:29:34 +0200

    solve constexpr problem

--- check_sudoku_valid.cpp
+++ check_sudoku_valid.cpp
@@ -2,6 +2,8 @@
 #include <algorithm>
 #include <bitset>
 using std::array;
+#include <iostream>
+using std::cout;
 
 
 bool check_mini_squares(const array<array<int,9>,9>& grid)
@@ -124,20 +126,20 @@ bool check_mini_squares(const array<array<int,9>,9>& grid)
 //check for horizontal lines validity
 constexpr bool check_lines(const array<array<int,9>,9>& board)
 {
-    std::array<int,9>numbers{ 0,0,0,0,0,0,0,0,0 };
-    for(const auto& element:board)
+    std::array<int,9>numbers{ 0,0,0,0,0,0,0,0,0};
+    for(size_t i=0;i<board.size();i++)
     {
-        for(const auto& j:element)
+        for(size_t j=0;j<board[0].size();j++)
         {
-            int temp=j;
+            int temp=board[i][j];
             temp--;
             if(numbers[temp]!=0)
             return false;
             numbers[temp]++;
         }
-        for(size_t i=0;i<numbers.size();i++)
+        for(size_t k=0;k<numbers.size();k++)
         {
-            numbers[i]=0;
+            numbers[k]=0;
         }
     }
     return true;
@@ -165,6 +167,15 @@ constexpr bool check_columns(const array<array<int,9>,9>& board)
 }
 int main()
 {
-  
-  
+
+    constexpr array<array<int,9>,9> sudoku{{{5,3,4,6,7,8,9,1,2},
+                                            {6,7,2,1,9,5,3,4,8},
+                                            {1,9,8,3,4,2,5,6,7},
+                                            {8,5,9,7,6,1,4,2,3},
+                                            {4,2,6,8,5,3,7,9,1},
+                                            {7,1,3,9,2,4,8,5,6},
+                                            {9,6,1,5,3,7,2,8,4},
+                                            {2,8,7,4,1,9,6,3,5},
+                                            {3,4,5,2,8,6,1,7,9}}};
+    cout<<check_mini_squares(sudoku);
 }
