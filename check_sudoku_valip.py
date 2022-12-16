4658f4d8eb8655736fd1e573213ac1c1f32059ec
ronsider <ronsider2@gmail.com>
2022-12-16 18:00:57 +0200

    checking columns of sudoku grid

--- check_sudoku_valip.py
+++ check_sudoku_valip.py
@@ -6,3 +6,12 @@ def valid_lines(grid):
         if(len(st) != 9):
             return False
     return True
+
+def valid_columns(grid):
+    for i in range(len(grid)):
+        st=set()
+        for j in range(len(grid)):
+            st.add(grid[j][i])
+        if(len(st)!=9):
+            return False    
+    return True 
